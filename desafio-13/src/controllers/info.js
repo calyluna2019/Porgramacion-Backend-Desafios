eae36b91460d6b7ecb7981cb23e704d2f7912ce1
calyluna2019 <calyluna87@gmail.com>
2022-07-25 17:26:07 -0300

    cambios desafio-13

---
+++ desafio-13/src/controllers/info.js
@@ -0,0 +1,21 @@
+const process = require('process');
+
+const info = (req, res) => {
+
+    const info = {
+        arguments:process.argv.slice(2),
+        platform:process.platform,
+        nodeVersion:process.version,
+        memoryTotalReserved:process.memoryUsage().rss,
+        execPath:process.execPath,
+        pid:process.pid,
+        proyectPath:process.cwd(),
+        cantCpu:core.cpus().length
+    }
+
+    res.render("info",info);
+}
+
+module.exports = {
+    info
+}
